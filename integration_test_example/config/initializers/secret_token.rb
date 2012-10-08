9153bdee5e065a2e5355faea28e7e9175483eeb4
Zack Drach <hogbait@gmail.com>
2012-10-08 17:40:48 -0400

    added integration test

---
+++ integration_test_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegrationTestExample::Application.config.secret_token = '957c1d75784d137978ac4c5004aa7b850db3e9c79f8b6108d6a71fb03810140655ec2e666733bbeba816b2563d74c52acc36312820d4dc5387ce81259016b4bf'
