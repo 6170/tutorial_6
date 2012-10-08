695a811eb94e28b74f03c6dcde319624aff32103
Zack Drach <hogbait@gmail.com>
2012-10-08 17:22:29 -0400

    simplify routes.rb

--- functional_test_example/test/functional/math_controller_test.rb
+++ functional_test_example/test/functional/math_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class MathControllerTest < ActionController::TestCase
-    test "should add when input is correct" do
+    test "add when input is correct" do
         get :add, { 'num1' => -2, 'num2' => 4}
         assert_response 200
         assert_equal "2", @response.body
         assert_equal "text/plain", @response.content_type
     end
-    test "should return 400 when params invalid" do
+    test "add should return 400 when params invalid" do
         get :add, {'num2' => 'blah'}
         assert_response 400
     end
