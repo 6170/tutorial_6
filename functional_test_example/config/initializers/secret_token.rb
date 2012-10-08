c108121aa80610ba3665960dae7120fc18251752
Zack Drach <hogbait@gmail.com>
2012-10-08 16:43:18 -0400

    initialize functional test

---
+++ functional_test_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunctionalTestExample::Application.config.secret_token = '931333ad94eff70c96ee36fe4bef4bcf59e2b16b2abda432016ee18e482a3eca868e74b690294a282e3bccc8eae67bea8102cf585a306439306799e962017bae'
