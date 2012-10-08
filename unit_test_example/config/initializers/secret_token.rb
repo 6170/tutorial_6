39203f08c86f611330b494b9117da6024ce53ac2
Zack Drach <hogbait@gmail.com>
2012-10-08 14:25:51 -0400

    initial commit

---
+++ unit_test_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnitTestExample::Application.config.secret_token = '16c47de031faeb1b6349e033673b95c752be7c8fab7329d545aed0b7a27003223e5ad4abef05ea7a2c4ad99486b9e874197e2535c440b65299cfa2229f0d42a0'
